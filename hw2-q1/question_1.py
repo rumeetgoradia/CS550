4de847a949134b710d2ad29d59e571362003fb95
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-03-04 17:59:41 -0500

    Add HW2 Q1

---
+++ hw2-q1/question_1.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy import linalg
+
+print("HW2 Q2 1.e.a")
+M = np.array([[1, 2], [2, 1], [3, 4], [4, 3]])
+U, sigma, Vt = linalg.svd(M, False)
+print("U", U)
+print("Sigma:", sigma)
+print("V^T", Vt)
+
+print("------------")
+
+print("HW2 Q2 1.e.b")
+Mt = np.transpose(M)
+Evals, Evecs = linalg.eigh(np.matmul(Mt, M))
+sortedIndices = np.flip(np.argsort(Evals))
+Evals = Evals[sortedIndices]
+Evecs = Evecs[:, sortedIndices]
+print("Evals:", Evals)
+print("Evecs:", Evecs)
